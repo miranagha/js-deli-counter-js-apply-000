6876c2c799d18c0184d5c0a7d08d059faa69969d
Miran Agha <m.agha46@gmail.com>
2018-04-25 23:07:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function nowServing (katzDeliLine){
     if(katzDeliLine.length===0) { 
     return 'There is nobody waiting to be served!'; 
 }
-    var customer = katzDeliLine.shift(); 
-    return "Currently serving " + customer + ".";
+    var person = katzDeliLine.shift(); 
+    return "Currently serving " + person + ".";
 }
