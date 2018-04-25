c14bf549f3b550d2d580412d429ce15a3f6c186a
Miran Agha <m.agha46@gmail.com>
2018-04-25 23:03:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function takeANumber(katzDeli, name) {
       return ('Welcome, ' + name + '. You are number ' + katzDeli.length + ' in line.')
   }
    takeANumber(katzDeli, 'Ada');
+function nowServing (katzDeliLine) { //declared function
+    if(katzDeliLine.length===0) { //checks if length of array(katzDeli) is = to 0
+    return 'There is nobody waiting to be served!'; //returns string no one is waiting
+}
+    var customer = katzDeliLine.shift(); // shift array of people
+    return "Currently serving " + customer + "."; //return string with customer name
+}
